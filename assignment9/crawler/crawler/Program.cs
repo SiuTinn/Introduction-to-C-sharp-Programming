8792ab77c9aa05a420709211029e969006152b5f
Siutinn <529187663@qq.com>
2024-04-24 16:29:46 +0800

    update assignment9

---
+++ assignment9/crawler/crawler/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SimpleCrawler
+{
+    class SimpleCrawler
+    {
+        private ConcurrentDictionary<string, bool> urls = new ConcurrentDictionary<string, bool>();
+        private int count = 0;
+
+        static void Main(string[] args)
+        {
+            SimpleCrawler myCrawler = new SimpleCrawler();
+            string startUrl = "http://www.cnblogs.com/dstang2000/";
+            if (args.Length >= 1) startUrl = args[0];
+            myCrawler.urls.TryAdd(startUrl, false); //加入初始页面
+            Task.Run(() => myCrawler.Crawl()).Wait();
+        }
+
+        private async Task Crawl()
+        {
+            Console.WriteLine("开始爬行了.... ");
+            while (true)
+            {
+                string current = null;
+                foreach (var url in urls.Keys)
+                {
+                    if ((bool)urls[url]) continue;
+                    current = url;
+                }
+
+                if (current == null || count > 10) break;
+
+                Console.WriteLine("爬行" + current + "页面!");
+                string html = await DownLoad(current); // 异步下载
+                urls[current] = true;
+                count++;
+                Parse(html); // 解析,并加入新的链接
+                Console.WriteLine("爬行结束");
+            }
+        }
+
+        public async Task<string> DownLoad(string url)
+        {
+            try
+            {
+                using (WebClient webClient = new WebClient())
+                {
+                    webClient.Encoding = Encoding.UTF8;
+                    string html = await webClient.DownloadStringTaskAsync(url);
+                    string fileName = count.ToString();
+                    File.WriteAllText(fileName, html, Encoding.UTF8);
+                    return html;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "";
+            }
+        }
+
+        private void Parse(string html)
+        {
+            string strRef = @"(href|HREF)[]*=[]*[""'][^""'#>]+[""']";
+            MatchCollection matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                strRef = match.Value.Substring(match.Value.IndexOf('=') + 1)
+                          .Trim('"', '\"', '#', '>');
+                if (strRef.Length == 0) continue;
+                urls.TryAdd(strRef, false);
+            }
+        }
+    }
+}
